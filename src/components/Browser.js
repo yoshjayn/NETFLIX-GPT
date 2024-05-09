e6a05a7b51cc4007cfc2f3493ca16ecd207f8a09
yoshjayn <aswiniy68@gmail.com>
2024-05-09 11:09:51 +0530

    9may

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import MainContainer from './MainContainer'
 
 const Browser = () => {
   return (
     <div>
     <Header />
-
+<MainContainer/>
     </div>
 
   )
