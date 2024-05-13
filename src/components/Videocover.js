86f25dd989eedd809b2877efd8d1821e28a1ee8a
yoshjayn <aswiniy68@gmail.com>
2024-05-13 09:47:05 +0530

    13May

--- src/components/Videocover.js
+++ src/components/Videocover.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Api_options } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTrailerVideo } from '../utils/movieSlice'
 
-const Videocover = () => {
+const Videocover = () => { debugger;
+    const trailerVideo = useSelector(store => store.movie?.trailerVideo);
+    console.log(trailerVideo)
+    const dispatch= useDispatch()
     const [trailerId, setTrailerId] = useState()
 
     const getMovieVideo=async()=>{ 
@@ -13,10 +18,11 @@ const Videocover = () => {
         })
         console.log("Your Trailer=", trailer)
         trailer = trailer.length? trailer : dataJs.results[0]
-        setTrailerId(trailer.key)
+        // setTrailerId(trailer[0].key)
         console.log(trailerId)
-    }
-    useEffect(()=>{
+        dispatch(addTrailerVideo(trailer[0]));
+          }
+    useEffect(()=>{ 
         getMovieVideo()
 
     },[])
@@ -24,9 +30,18 @@ const Videocover = () => {
     
   return (
     <div>
-  <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerId} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
+    {trailerVideo? <div>
+      <iframe width="560" 
+    height="315" 
+    src={"https://www.youtube.com/embed/"+trailerVideo.key} 
+    title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
+      </div> : 
+    <div> Yashaswini
+    </div>
+    }
     </div>
   )
 }
 
 export default Videocover
+
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -5,14 +5,18 @@ const movieSlice=createSlice(
         name:"movieSlice",
         initialState:{
             nowPlaying : null,
+            trailerVideo: null
         },
         reducers:{
             addNowPLaying:(state,action)=>{
                 state.nowPlaying=action.payload
+            },
+            addTrailerVideo:(state,action)=>{
+                state.trailerVideo=action.payload
             }
         }
     }
 )
 
-export const { addNowPLaying}=movieSlice.actions;
+export const { addNowPLaying , addTrailerVideo} = movieSlice.actions;
 export default movieSlice.reducer;
